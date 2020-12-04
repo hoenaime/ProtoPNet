a91e9ed795d4cf2333b50435f8bf6c46ee2ed936
Mimoun Mohamed <mim.mohamed33@gmail.com>
2020-12-04 16:24:52 +0100

    fix folder train test split

--- train_test_split.py
+++ train_test_split.py
@@ -23,8 +23,9 @@ if __name__ == '__main__':
             img_path = dataset_cropped_path / img_location
             is_training_img = int(test_train_line.split(" ")[1]) == 1
             if is_training_img:
-                output_location = train_folder / img_path.name
+                output_location = train_folder / img_location
             else:
-                output_location = test_folder / img_path.name
+                pass
+                output_location = test_folder / img_location
             output_location.parent.mkdir(parents=True, exist_ok=True)
             shutil.copy(img_path.as_posix().strip(), output_location.as_posix().strip())
