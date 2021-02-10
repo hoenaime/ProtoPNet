acc9c0c8939275355041826eb19c07169d4030b0
Mimoun Mohamed <mim.mohamed33@gmail.com>
2021-02-10 12:49:26 +0100

    Fix aug_img p&

--- img_aug.py
+++ img_aug.py
@@ -21,6 +21,9 @@ target_folders = [os.path.join(target_dir, folder) for folder in next(os.walk(di
 for i in range(len(folders)):
     fd = folders[i]
     tfd = target_folders[i]
+    if os.path.isdir(tfd):
+        print("Déjà augmenté: " + tfd)
+        continue
     # rotation
     p = Augmentor.Pipeline(source_directory=fd, output_directory=tfd)
     p.rotate(probability=1, max_left_rotation=15, max_right_rotation=15)
