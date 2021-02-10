66d3090372d16afb0e784b556030ae8f995ecf15
Mimoun Mohamed <mim.mohamed33@gmail.com>
2021-02-10 13:06:20 +0100

    fix img aug

--- img_aug.py
+++ img_aug.py
@@ -21,10 +21,8 @@ target_folders = [os.path.join(target_dir, folder) for folder in next(os.walk(di
 for i in range(len(folders)):
     fd = folders[i]
     tfd = target_folders[i]
-    print(os.path.join(datasets_root_dir, os.path.basename(tfd)))
-    print(os.getcwd())
-    print(tfd)
-    if os.path.isdir(os.path.join(datasets_root_dir, os.path.basename(tfd))[2:]):
+    print(os.path.join(fd, os.path.basename(tfd)))
+    if os.path.isdir(os.path.join(fd, os.path.basename(tfd))):
         print("Déjà augmenté: " + tfd)
         continue
     # rotation
